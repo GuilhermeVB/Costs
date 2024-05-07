a0b38583db39d7af99ce865db6ff531664bb4448
Guilherme <guibbs11032003@gmail.com>
2024-05-07 17:50:23 -0300

    Project Import

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,31 @@
+import { useLocation } from "react-router-dom"
+
+import Message from "../layout/Message"
+import Container from "../layout/Container"
+
+import styles from './Projects.module.css'
+import LinkButton from "../layout/LinkButton"
+
+function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+            {message && <Message type="sucess" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+        </div>
+    )
+}
+
+export default Projects
