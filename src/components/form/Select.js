494ad74584381af88f5f9c7b96dac585528bebca
Guilherme <guibbs11032003@gmail.com>
2024-05-10 15:09:21 -0300

    Criação de Loading, Cards e Página de edição de projeto. E adição dos required Input e Select

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,7 +4,7 @@ function Select({ text, name, options, handleOnChange, value }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
-            <select
+            <select required
                 name={name}
                 id={name}
                 onChange={handleOnChange}
@@ -12,7 +12,7 @@ function Select({ text, name, options, handleOnChange, value }) {
             >
                 <option>Selecione uma opção</option>
                 {options.map((option) => (
-                    <option value={option.id} key={option.id}>
+                    <option required value={option.id} key={option.id}>
                         {option.name}
                     </option>
                 ))}
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,56 @@
 import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 import Message from "../layout/Message"
 import Container from "../layout/Container"
+import Loading from "../layout/Loading"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
 
 import styles from './Projects.module.css'
-import LinkButton from "../layout/LinkButton"
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveloading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:5000/projects", {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    setProjects(data)
+                    setRemoveloading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 300)
+    }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(resp => resp.json())
+            .then(() => {
+                setProjects(projects.filter((project) => project.id !== id))
+                setProjectMessage('Projeto removido com sucesso!')
+            })
+            .catch((err) => console.log(err))
     }
 
     return (
@@ -21,8 +60,24 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="sucess" msg={message} />}
+            {projectMessage && <Message type="sucess" msg={projectMessage} />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                            handleRemove={removeProject}
+                        />
+                    ))
+                }
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
